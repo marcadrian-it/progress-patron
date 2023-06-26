c59794e03d4e47ba0bd8065b861c82711e5fc722
marcadrian-it <marcadrian.it@gmail.com>
2023-06-26 14:39:04 +0200

    fix border loading

--- app/(dashboard)/issues/loading.tsx
+++ app/(dashboard)/issues/loading.tsx
@@ -4,7 +4,7 @@ export default function HomePageLoader() {
   return (
     <div className="flex justify-center items-center w-full h-full">
       <Card className="">
-        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-violet-400"></div>
       </Card>
     </div>
   );
