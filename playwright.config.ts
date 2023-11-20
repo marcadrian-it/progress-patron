906694d57cfa6d355162944caebc668dce36c133
marcadrian-it <marcadrian.it@gmail.com>
2023-11-20 17:55:15 +0100

    workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "yarn start > server.log 2>&1",
-    url: "http://127.0.0.1:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "yarn start",
+  //   url: "http://127.0.0.1:3000",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
