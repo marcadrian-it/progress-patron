448a509b1bf8100f7a05c0fab9d200b58f7cb95a
marcadrian-it <marcadrian.it@gmail.com>
2024-05-14 15:01:35 +0200

    cicd: update playwright test

--- __tests__/auth.spec.ts
+++ __tests__/auth.spec.ts
@@ -11,11 +11,14 @@ test("it has an input field for logging in", async ({ page }) => {
 test("auth works correctly", async ({ page }) => {
   test.setTimeout(60000);
   await page.goto("/signin", { waitUntil: "networkidle" });
-  const emailField = page.getByPlaceholder("E-mail");
-  const passwordField = page.getByPlaceholder("Password");
+  const emailField = await page.waitForSelector('[placeholder="E-mail"]');
+  const passwordField = await page.waitForSelector('[placeholder="Password"]');
   await emailField.fill("demo@demo.com");
   await passwordField.fill("password");
-  await page.getByText("Sign In").click();
+  await Promise.all([
+    page.waitForLoadState("networkidle"),
+    page.click('text="Sign In"'),
+  ]);
 
   await expect(page).toHaveURL(/.*home/, { timeout: 15000 });
 });
