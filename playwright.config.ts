7696ddea97d1fbbb966f780af38024f6282c7fb8
marcadrian <marcadrian.it@gmail.com>
2024-08-08 21:17:56 +0200

    chore: reorganize test structure and config (#28)
    * chore: reorganize test structure and config
    
    * Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,8 @@ export default defineConfig({
     screenshot:
       "only-on-failure" /* Capture screenshots only for failed tests */,
   },
-  testDir: "./playwright/__tests__",
+  testDir: "./__tests__/playwright",
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
